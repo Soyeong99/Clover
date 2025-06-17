264270c9250bc20a899a31ad59a3f054457871ea
Soyeong99 <soyeongk99@gmail.com>
2025-06-17 16:52:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+  document.addEventListener('DOMContentLoaded', function () {
+
+  
+  // <!-- fixed_cta / cta 영역 스크롤 감지 -->
+  const fixedCTA = document.querySelector('.fixed_cta');
+  const ctaArea = document.querySelector('#formArea');
+  let isHidden = false;
+
+  if (fixedCTA && ctaArea) {
+    window.addEventListener('scroll', () => {
+      const ctaRect = ctaArea.getBoundingClientRect();
+      const offset = 100; // 사라지는 지점을 조금 아래로
+
+      if (ctaRect.top < window.innerHeight - offset) {
+        if (!isHidden) {
+          fixedCTA.classList.add('fade-out');
+          isHidden = true;
+        }
+      } else {
+        if (isHidden) {
+          fixedCTA.classList.remove('fade-out');
+          isHidden = false;
+        }
+      }
+    });
+  }
+
+});
